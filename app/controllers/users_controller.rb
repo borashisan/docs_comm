66e834bfefa3888a0232072a5941c467f9a3f5f1
borashisan <tomochuro731@gmail.com>
2021-03-23 00:28:16 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
-      log_in @user
-      flash[:success] = "登録が完了しました"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "メールを確認してアカウントを有効化してください"
+      redirect_to root_url 
     else
       render 'new'
     end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'cd5c23bd32fd4a989da0a8bd1ffbc81f.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
