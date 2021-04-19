a27317c1c7325664ae2f82f3e4a0c502de3cb5c8
borashisan <tomochuro731@gmail.com>
2021-04-19 11:59:50 +0000

    -

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  VALID_CONTENT_REGEX = /\A(https\:\/\/docs\.google\.com\/forms\/d\/e\/)+([\w+\-.]{56})+(\/viewform|\/viewform[\w+\-.?=]{1,})\z/
+  VALID_CONTENT_REGEX = /\A(https\:\/\/docs\.google\.com\/forms\/d\/e\/|https\:\/\/docs\.google\.com\/forms\/d)+([\w+\-.]{56})+(\/viewform)+([\w+\-.?=]{1,}*)\z/
   validates :content, presence: true, length: { in: 90..140 },
   format: { with: VALID_CONTENT_REGEX }
 end
