54fce18a1ff4584532d1dce3b1bae596ea93ce94
borashisan <tomochuro731@gmail.com>
2021-04-19 10:19:53 +0000

    google valitate

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,9 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { in: 90..140 }
+  VALID_CONTENT_REGEX = /\A(https\:\/\/docs\.google\.com\/forms\/d\/e\/)+([\w\-.]{56})+(\/viewform)\z/
+  validates :content, presence: true, length: { in: 90..140 },
+  format: { with: VALID_CONTENT_REGEX }
 end
+ 
  
