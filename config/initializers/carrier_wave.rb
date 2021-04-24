c12c3d6bd71fd3f8f8bfb53ce3ca8922e166a523
borashisan <tomochuro731@gmail.com>
2021-04-24 22:31:19 +0000

    -

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIA4HFKPDHMEVG7N3RJ '],
+      :aws_access_key_id     => ENV['AKIA4HFKPDHMEVG7N3RJ'],
       :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['docscomm']
