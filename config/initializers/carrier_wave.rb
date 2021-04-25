2487cc4f4551133ae614f785adea56e40ec764f8
borashisan <tomochuro731@gmail.com>
2021-04-25 02:10:24 +0000

    -

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,8 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'docs-comm' # 作成したバケット名を記述
@@ -10,11 +11,11 @@ CarrierWave.configure do |config|
       # Amazon S3用の設定
       provider: 'AWS',
       region: 'ap-northeast-1',     # 例: 'ap-northeast-1'
-      aws_access_key_id: ENV['AKIA4HFKPDHMEVG7N3RJ'],
-      aws_secret_access_key: ENV['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'],
+      aws_access_key_id: 'AKIA4HFKPDHMEVG7N3RJ',
+      aws_secret_access_key: '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',
       path_style: true
     }
     
     
-
+  end
 end
