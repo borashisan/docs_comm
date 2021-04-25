bbb8e6f710cad542e619ea58acea375c1a6edee8
borashisan <tomochuro731@gmail.com>
2021-04-25 02:47:58 +0000

    -

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,8 +11,8 @@ if Rails.env.production?
       # Amazon S3用の設定
       provider: 'AWS',
       region: 'ap-northeast-1',     # 例: 'ap-northeast-1'
-      aws_access_key_id: 'AKIA4HFKPDHMLXECN72C',
-      aws_secret_access_key: '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',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
       path_style: true
     }
     
