586ecc16cfbb66e22c99dabf4207cd0f135c95bf
borashisan <tomochuro731@gmail.com>
2021-05-01 02:20:26 +0000

    -

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  enum micropost_sex: { man: 0, woman:1, nonsex:2}
+  enum micropost_sex: { man: 0, woman:1, nonsex:2, NULL:3 }
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   VALID_CONTENT_REGEX = /\A(https\:\/\/docs\.google\.com\/forms\/d\/e\/)+([\w+\-.]{56})+(\/viewform)+([\w+\-.?=]{1,20}*)\z/
